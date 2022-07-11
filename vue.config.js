96a72edfbf6b16f4a9e6a0e2cd2ab12be6dc70b1
Kiril Delovski <delovski.office@gmail.com>
2022-07-11 17:14:35 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/frontend-coding-challenge' : '/',
+    publicPath: process.env.NODE_ENV === 'production' ? '/vue-nasa-rover-images-infinite-scroll' : '/',
 };  
